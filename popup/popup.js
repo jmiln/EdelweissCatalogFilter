0f0af3c24fd3687faceb1b46d168d6b6c2dc81f7
Jeff Milner <j.milner359@gmail.com>
2021-08-16 15:25:12 -0700

    Bit of cleanup

--- popup/popup.js
+++ popup/popup.js
@@ -1,37 +1,8 @@
-function filter() {
-    browser.tabs.executeScript({file: "../scripts/filterCats.js"});
+const files = ["filterCats", "searchCats", "tagView", "getIsbns", "getTitleListInfo"];
+
+for (const file of files) {
+    thisButton = document.getElementById(file);
+    thisButton.addEventListener("click", () => {
+        browser.tabs.executeScript({file: `../scripts/${file}.js`});
+    })
 }
-
-function search() {
-    browser.tabs.executeScript({file: "../scripts/searchCats.js"});
-}
-
-function tagView() {
-    browser.tabs.executeScript({file: "../scripts/tagView.js"});
-}
-
-function getIsbns() {
-    browser.tabs.executeScript({file: "../scripts/getIsbns.js"});
-}
-
-function getTitleInfo() {
-    browser.tabs.executeScript({file: "../scripts/getTitleListInfo.js"});
-}
-
-// Button to filter the catalogs based on the saved filters
-filterCatButton = document.getElementById("filterCats");
-filterCatButton.addEventListener("click", filter);
-
-// Button to filter the catalogs to only show the one(s) you search for
-filterCatButton = document.getElementById("searchCats");
-filterCatButton.addEventListener("click", search);
-
-// Button to change the tags/ notes/ reviews to show in a textArea, so they can be copied
-textifyButton = document.getElementById("textifyButton");
-textifyButton.addEventListener("click", tagView);
-
-isbnsButton = document.getElementById("isbnsButton");
-isbnsButton.addEventListener("click", getIsbns);
-
-titleGrabButton = document.getElementById("titleGrabButton");
-titleGrabButton.addEventListener("click", getTitleInfo);
--- scripts/getIsbns.js
+++ scripts/getIsbns.js
@@ -1,20 +1,24 @@
 function getIsbns() {
-    // Get the html chunks that have the isbns in them
-    isbnFields = document.querySelectorAll(".pve_sku ")
-
-    if (!isbnFields?.length) {
-        // If there were none, alert the user
-        alert("I could not find any valid ISBN fields on this page")
+    if (document.getElementById("listTypeIcon_3").classList.contains("displaySelected")) {
+        alert("Please make sure you're not viewing these titles in condensed mode.");
     } else {
-        // Otherwise, parse out the isbns to just the 13 digit since they seem to have both, comma seperated, and send an alert 
-        isbns = [];
-        isbnFields.forEach(p => isbns.push(p.innerText.split(",")[0]));
-        if (isbns.length) {
-            alert(isbns.join("\n"));    
+        // Get the html chunks that have the isbns in them
+        isbnFields = document.querySelectorAll(".pve_sku ")
+
+        if (!isbnFields?.length) {
+            // If there were none, alert the user
+            alert("I could not find any valid ISBN fields on this page")
         } else {
-            alert("I could not find any ISBNs on this page.")
+            // Otherwise, parse out the isbns to just the 13 digit since they seem to have both, comma seperated, and send an alert 
+            isbns = [];
+            isbnFields.forEach(p => isbns.push(p.innerText.split(",")[0]));
+            if (isbns.length) {
+                alert(isbns.join("\n"));
+            } else {
+                alert("I could not find any ISBNs on this page.")
+            }
         }
     }
 }
-
 getIsbns();
+
