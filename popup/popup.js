733da6d52916a88b19e90468e0c5c88a13a7d81b
Jeff Milner <j.milner359@gmail.com>
2021-08-16 15:36:54 -0700

    Made the script loading more dynamic

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,34 @@
-const files = ["filterCats", "searchCats", "tagView", "getIsbns", "getTitleListInfo"];
+const files = [
+    {
+        fileName: "filterCats",
+        buttonText: "Filter Cats"
+    },
+    {
+        fileName: "searchCats",
+        buttonText: "Search Cats"
+    },
+    {
+        fileName: "tagView",
+        buttonText: "Change Tags"
+    },
+    {
+        fileName: "getIsbns",
+        buttonText: "Get ISBNs"
+    },
+    {
+        fileName: "getTitleListInfo",
+        buttonText: "Get Book Info"
+    }
+]
+const popupHtml = document.getElementById("popup-content");
 
 for (const file of files) {
-    thisButton = document.getElementById(file);
+    thisButton = document.createElement("div");
+    thisButton.type = "button";
+    thisButton.innerHTML = file.buttonText;
+    thisButton.className = "button";
     thisButton.addEventListener("click", () => {
-        browser.tabs.executeScript({file: `../scripts/${file}.js`});
-    })
+        browser.tabs.executeScript({file: `../scripts/${file.fileName}.js`});
+    });
+    popupHtml.appendChild(thisButton);
 }
